cd8c8e3a84baae850820148e5c58d0c8dd9a2e33
ShUbHaM13M <shubham.heeralal@gmail.com>
2023-08-15 20:44:06 +0530

    Tank sprite and rotation

--- game.py
+++ game.py
@@ -19,7 +19,7 @@ class Game:
         self.player.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
-        pg.display.set_caption(f"{self.clock.get_fps()} :1.f")
+        pg.display.set_caption(f"{self.clock.get_fps():.1f}")
 
     def draw(self):
         self.screen.fill(BG)
--- tank.py
+++ tank.py
@@ -1,45 +1,97 @@
 import pygame as pg
-from settings import RES, PLAYER_WIDTH, PLAYER_HEIGHT
+from settings import RES
+import math
+import os
+
+
+def clamp(num, min, max):
+    return min if num < min else max if num > max else num
 
 
 class Tank:
     def __init__(self, game) -> None:
         self.game = game
         self.x = RES[0] * 0.5
-        self.y = RES[0] * 0.5
-        self.speed = 0.5
+        self.y = RES[1] * 0.5
+        self.scale_factor = 0.6
+        self.angle = 0
 
-    def check_bounds(self):
-        if self.x - PLAYER_WIDTH * 0.5 < 0:
-            self.x = 0
-        elif self.x + PLAYER_WIDTH > RES[0]:
-            pass
+        self.image = pg.image.load(
+            os.path.join("assets", "tankBody_blue.png")
+        ).convert()
+        self.image = pg.transform.rotate(
+            pg.transform.scale_by(self.image, (self.scale_factor, self.scale_factor)),
+            90,
+        )
+        self.image.set_colorkey((0, 0, 0))
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+        self.barrel_image = pg.image.load(
+            os.path.join("assets", "tankBlue_barrel2.png")
+        ).convert()
+        self.barrel_image = pg.transform.scale_by(self.barrel_image, (0.8, 0.8))
+
+        self.barrel_image.set_colorkey((0, 0, 0))
+
+        self.speed = 0.5
+        self.velocity = [0, 0]
+        self.gun_angle = 0
 
     def render(self):
-        pg.draw.rect(
-            self.game.screen,
-            "red",
+        self.game.screen.blit(
+            pg.transform.rotate(self.image, self.angle),
+            (self.x - self.width * 0.5, self.y - self.height * 0.5),
+        )
+        self.game.screen.blit(
+            pg.transform.rotate(self.barrel_image, self.gun_angle),
             (
-                self.x - PLAYER_WIDTH * 0.5,
-                self.y - PLAYER_WIDTH * 0.5,
-                PLAYER_WIDTH,
-                PLAYER_HEIGHT,
+                self.x - self.barrel_image.get_width() * 0.5,
+                self.y,
             ),
         )
 
     def update(self):
-        self.check_events()
+        self.movement()
+        self.gun_movement()
 
-    def check_events(self):
+    def gun_movement(self):
+        mouse_pos = pg.mouse.get_pos()
+        angle = math.atan2(
+            mouse_pos[1] - self.y,
+            mouse_pos[0] - self.x - self.barrel_image.get_width() * 0.5,
+        )
+        self.gun_angle = angle * (180 / math.pi)
+
+    def movement(self):
         keys = pg.key.get_pressed()
         delta_time = self.game.delta_time
 
         if keys[pg.K_d]:
-            self.x += self.speed * delta_time
+            self.velocity[0] = 1
+            self.angle = 0
         elif keys[pg.K_a]:
-            self.x -= self.speed * delta_time
+            self.velocity[0] = -1
+            self.angle = 180
 
         elif keys[pg.K_w]:
-            self.y -= self.speed * delta_time
+            self.velocity[1] = -1
+            self.angle = 90
         elif keys[pg.K_s]:
-            self.y += self.speed * delta_time
+            self.velocity[1] = 1
+            self.angle = 270
+
+        self.x += self.velocity[0] * self.speed * delta_time
+        self.y += self.velocity[1] * self.speed * delta_time
+
+        self.x = clamp(
+            self.x,
+            self.width * 0.5,
+            RES[0] - self.width * 0.5,
+        )
+        self.y = clamp(
+            self.y,
+            self.height * 0.5,
+            RES[1] - self.height * 0.5,
+        )
+        self.velocity = [0, 0]
